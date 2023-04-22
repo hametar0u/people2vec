26cb2ed6250c7b4217f177f3ffe3a306b1682201
hametar0u <57603310+hametar0u@users.noreply.github.com>
2023-04-21 23:16:06 -0700

    db with basic user class

--- server/app.py
+++ server/app.py
@@ -1,7 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    vec = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+    
+app.app_context().push()
