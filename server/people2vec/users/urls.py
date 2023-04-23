7da82b1eeb673b56f3887593fdda1af9f0e0fb95
hametar0u <57603310+hametar0u@users.noreply.github.com>
2023-04-22 22:29:29 -0700

    Update urls.py

--- server/people2vec/users/urls.py
+++ server/people2vec/users/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login, name="login"),
     path("signup", views.signup, name="signup"),
+    path("get_user_data", views.get_user_data, name="get_user_data"),
 ]
